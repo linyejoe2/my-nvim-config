0cda532aaa93bb30ba2cd1a9f4063db0ada0bd84
linyejoe2 <linyejoe2@gmail.com>
2025-01-10 10:44:33 +0800

    mapping key: 'm -> Home', ', -> End', '<A-m> -> PageUp', '<A-,> -> PaguDown'

--- lua/linyejoe2/remap.lua
+++ lua/linyejoe2/remap.lua
@@ -26,8 +26,11 @@ vim.keymap.set("x", "<leader>p", "\"_dp")
 -- linyejoe2
 
 vim.keymap.set("n", "<leader>s", ":echo 'adsf'<CR>")
-vim.keymap.set("n", "m", "<C-u>zz")
-vim.keymap.set("n", ",", "<C-d>zz")
+vim.keymap.set("n", "<A-m>", "<C-u>zz")
+vim.keymap.set("n", "<A-,>", "<C-d>zz")
+
+vim.keymap.set("n", "m", "<Home>")
+vim.keymap.set("n", ",", "<End>")
 
 vim.keymap.set("n", "j", "k")
 vim.keymap.set("n", "k", "j")
