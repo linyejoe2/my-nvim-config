a544830fa7dc171598f936729f0ee189e5712bc0
linyejoe2 <linyejoe2@gmail.com>
2025-01-11 22:47:39 +0800

    add find hiding files to key binding as <leader>fh

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fh', ":Telescope find_files hidden=true<CR>", { desc = 'Telescope find hiding files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
