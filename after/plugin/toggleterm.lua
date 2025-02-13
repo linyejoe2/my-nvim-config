8048a2b69559b641fdae2caa9e0bdbfb6b818d90
linyejoe2 <linyejoe2@gmail.com>
2025-02-13 17:11:32 +0800

    change toggleterm to float windows

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,12 +1,11 @@
 print("load toggleterm")
 
-require("toggleterm").setup{
-  size = 12,
-  open_mapping = [[<C-\>]],
-  direction = 'horizontal',  -- 使用浮動終端
-  close_on_exit = true,     -- 當終端退出時自動關閉窗口
-  shade_terminals = true,   -- 讓終端窗口的顏色稍微變暗，以區分普通編輯區域
-  shading_factor = '-30',
-  shading_ratio = '-3',
+require("toggleterm").setup {
+	size = 50,
+	open_mapping = [[<C-\>]],
+	direction = 'float', -- 使用浮動終端
+	close_on_exit = true,  -- 當終端退出時自動關閉窗口
+	shade_terminals = true, -- 讓終端窗口的顏色稍微變暗，以區分普通編輯區域
+	shading_factor = '-30',
+	shading_ratio = '-3',
 }
-
