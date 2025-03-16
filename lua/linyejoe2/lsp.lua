e24a4eeb337d06468e03727db2b84baf8c11898d
linyejoe2 <linyejoe2@gmail.com>
2025-03-16 13:25:24 +0800

    add css_lsp, html_lsp to nvim lspconfig default install option.

--- lua/linyejoe2/lsp.lua
+++ lua/linyejoe2/lsp.lua
@@ -40,7 +40,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
 	-- Replace the language servers listed here
 	-- with the ones you want to install
-	ensure_installed = { 'lua_ls', 'gopls', 'ts_ls', 'biome', "clangd" },
+	ensure_installed = { 'lua_ls', 'gopls', 'ts_ls', 'biome', "clangd", "cssls", "html" },
 	handlers = {
 		function(server_name)
 			require('lspconfig')[server_name].setup({})
